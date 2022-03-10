d62077c2257bf204c2f6a7dddf874ebe91667d8c
Alex Prieu <daprieu@gmail.com>
2022-03-10 10:15:54 -0600

    removed bootstrap imports

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./NavBar.css"
-import bootstrap from 'bootstrap'
+
 import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap'
 
 export const MWNavBar = () => {
