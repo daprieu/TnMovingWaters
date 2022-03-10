c60e8e2a5faf85c9efe7b91a0be66ef2bb39c152
Alex Prieu <daprieu@gmail.com>
2022-03-10 07:39:00 -0600

    added new font and fixed font sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Footer.css'
-
+import { BsFacebook, BsInstagram, BsYoutube } from 'react-icons/bs';
 const Footer = () => {
   return (
       <>
         <div className="footer">
-            <div>Tennessee Moving Waters Guide Service</div>
+            <span><p>Tennessee Moving Waters Guide Service</p></span>
             <div>125 Sunnymeade Dr, Mt. Juliet, TN 37122</div>
             <div>Phone: (615) 506-2951</div>
-            
+            <div className='links'>
+            <a href="https://www.instagram.com/tnmovingwaters/?hl=en"><BsInstagram /></a>
+            <a href="https://www.facebook.com/tnmovingwatersguideservice/"><BsFacebook /></a>
+            <a href="https://www.youtube.com/channel/UCr3wIjfwL74OWshmIRkfevg"><BsYoutube /></a>
+            </div>
         </div>
         
       </>
