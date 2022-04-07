620e0b3ec40bc5e5a2cadf362e038149fd25d85a
Alex Prieu <daprieu@gmail.com>
2022-04-07 17:15:51 -0500

    removed validation and fixed iframe title

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm } from '@formspree/react';
 import { Button, Container, Form, Row, Col} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './ContactUs.css'
--- src/components/FishingReports.js
+++ src/components/FishingReports.js
@@ -18,7 +18,7 @@ const FishingReports = () => {
       </Col>
       </Row>
       
-      <iframe className='facebookIFrame'  ctitle="TN Moving Waters Facebook page" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftnmovingwatersguideservice&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" 
+      <iframe className='facebookIFrame'  title="TN Moving Waters Facebook page" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftnmovingwatersguideservice&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" 
        height="500" style={{border:"none", overflow:"hidden"}} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </Container>
     
