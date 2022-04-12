251b1f397d48e6b7577eca0aa75296894be7cda6
Alex Prieu <daprieu@gmail.com>
2022-04-12 13:03:33 -0500

    changed links to correct destination on sponsor image links

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -9,17 +9,17 @@ const Sponsors = () => {
       <h2>Our Sponsors</h2>
       <Row className='d-flex py-2'>
         <Col className='sponsor d-flex align-items-center justify-content-center'>
-        <a href="https://bullshad.com/" aria-label="Bullshad website">
+        <a href="https://stcroixrods.com/" aria-label="St. Croix Rods website">
           <img src="https://res.cloudinary.com/alexprieuweb/image/upload/c_scale,f_webp,w_300/v1649785215/TNMW/st._croix_rods_vcjkbs.webp" alt="St. croix rods"/>
           </a>
         </Col>
         <Col className='sponsor d-flex align-items-center justify-content-center'>
-        <a href="https://bullshad.com/" aria-label="Bullshad website">
+        <a href="https://bonafidekayaks.com/" aria-label="Bonafide Kayaks website">
           <img src="https://res.cloudinary.com/alexprieuweb/image/upload/c_scale,f_webp,w_300/v1649785215/TNMW/bonafide-logo-white_uynsad.webp" alt="Bonafide kayaks"/>
           </a>
         </Col>
         <Col className='sponsor d-flex align-items-center justify-content-center'>
-        <a href="https://bullshad.com/" aria-label="Bullshad website">
+        <a href="https://luckycraft.com/luckycrafthome/default.htm" aria-label="Lucky Craft website">
           <img src="https://res.cloudinary.com/alexprieuweb/image/upload/c_scale,f_webp,w_300/v1649785215/TNMW/lucky_craft_1024x1024_b03aaf.webp" alt="Lucky Craft"/>
           </a>
         </Col>
